2ff783308eb608cb162769006334912a997c5bc1
Cheetttos <20030425@itcelaya.edu.mx>
2023-10-09 11:57:34 -0600

    nuevo

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const conn = require('./libs/MongooseConnection')
-//const employeeRoutes = require('./routes/employeeRoutes')
+const productRoutes = require('./routes/productRoutes')
 const app = express()
 app.use(express.json())
 const path = process.env.API_PREFIX
-//app.use(path, employeeRoutes)
+app.use(path, productRoutes)
+
 const port = process.env.PORT
 
 app.listen(port, async () => {
     console.log(`Example app listening on port ${port}!`)
 })
-
--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,10 +1,8 @@
-
-const { Long } = require('mongodb');
 const mongoose = require('mongoose');
 const CategorySchema = new mongoose.Schema({
     id : Number,
     name: String
-});
+},
+{collection:'category'});
 const Category = mongoose.model('Category', CategorySchema);
 module.exports=Category
-
